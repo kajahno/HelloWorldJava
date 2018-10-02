2157003302d718517d6040adadc1c0d8f3c2361a
dave-pollock <david.pollock@judopayments.com>
2018-10-02 17:57:04 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # HelloWorldJava
+
+## Pre-Requisites
+ - Gradle
+ - JDK 8
+ 
+Or
+ - Docker
+
+## Building
+### With Docker
+1. `docker-compose run build`
+
+### With Gradle
+1. `gradle bootJar`
+
+## Running
+### With Docker
+1. `docker-compose build app`
+1. `docker-compose up -d app`
+
+### With Java
+1. `java -jar build/libs/HelloWorldJava-0.1.0.jar`
+
+## Test
+`curl http://localhost:8080/`
---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public Greeting greeting() {
+      return new Greeting("Hello World!");
+    }
+}
